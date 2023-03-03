3e1948cc3af7c6ebd8cbb2d4f008b6f2bad517c0
pamcarvalho-moore <pcarvalh@email.uark.edu>
2023-03-03 11:18:07 -0600

    adding genome db file and script

--- script.sh
+++ script.sh
@@ -5,3 +5,8 @@
 #SBATCH --nodes=4
 #SBATCH --tasks-per-node=32
 #SBATCH --time=00:00:10
+
+module load blast/2.13.0+bin
+
+makeblastdb -i Saccha_genome.fna -dbtype nucl -parse_seqids -out Saccha_genome.fna -title "Saccha_genome.fna db"
+
